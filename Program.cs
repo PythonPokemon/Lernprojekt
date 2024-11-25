512a2e6537b0e3f2816b6c7cfafed7f5374d594b
jakob.derzapf <Jakob.D.Baron@outlook.de>
2024-11-25 18:23:50 +0100

    Architektur erweitert

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Lernprojekt.Themen.Monat_1_Grundlagen_von_Datenstrukturen.Arrays; // Namespace importieren
+using Lernprojekt.Themen.Monat_2_Fortgeschrittene_Datenstrukturen;
+using Lernprojekt.Themen.Monat_3_Such_und_Sortieralgorithmen;
+using Lernprojekt.Themen.Monat_4_Baumstrukturen;
+using Lernprojekt.Themen.Monat_5_Graphen_und_Algorithmen;
+using Lernprojekt.Themen.Monat_6_Praxisprojekte_und_Optimierung;
 
 namespace Lernprojekt
 {
@@ -14,6 +19,11 @@ namespace Lernprojekt
             Console.WriteLine("Tag 1: Arrays Grundlagen");
             Tag_1.Lernen(); // Aufruf der Methode aus der Klasse Tag_1
 
+            Test2.Test2lauf();
+            Test3.Test3lauf();
+            Test4.Test4lauf();
+            Test5.Test5lauf();
+            Test6.Test6lauf();
 
             // Konsole offen halten
             Console.WriteLine("Drücken Sie eine beliebige Taste, um das Programm zu beenden.");
---
+++ Themen/Monat_4_Baumstrukturen/Test4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lernprojekt.Themen.Monat_4_Baumstrukturen
+{
+    internal class Test4
+    {
+        public static void Test4lauf ()
+        {
+
+            // 1. Ein Array deklarieren und initialisieren
+            int[] zahlen = { 10, 20, 30, 40, 50 };
+
+            // 2. Zugriff auf ein Element des Arrays mit seinem Index
+            Console.WriteLine("Erstes Element: " + zahlen[0]);
+
+            // 3. Länge des Arrays anzeigen
+            Console.WriteLine("Länge des Arrays: " + zahlen.Length);
+
+            // 4. Alle Elemente des Arrays durchlaufen
+            Console.WriteLine("Alle Elemente im Array:");
+            for (int i = 0; i < zahlen.Length; i++)
+            {
+                Console.WriteLine("Index " + i + ": " + zahlen[i]);
+            }
+
+            // 5. Elemente bearbeiten
+            zahlen[1] = 25; // Das Element an Index 1 wird geändert.
+            Console.WriteLine("Geändertes Array:");
+            foreach (var zahl in zahlen)
+            {
+                Console.WriteLine(zahl);
+            }
+        }
+    }
+}
