e0c72340e292e65fd7e57a2085e9990e2cd318a5
jakob.derzapf <Jakob.D.Baron@outlook.de>
2024-11-25 16:54:13 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lernprojekt.Themen.Arrays; // Namespace importieren
+
+namespace Lernprojekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tag 1: Arrays Grundlagen");
+            Tag_1.Lernen(); // Aufruf der Methode aus der Klasse Tag_1
+
+
+            // Konsole offen halten
+            Console.WriteLine("Drücken Sie eine beliebige Taste, um das Programm zu beenden.");
+            Console.ReadKey();
+
+        }
+    }
+}
